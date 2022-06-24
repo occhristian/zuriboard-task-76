2de9eda48e2efff368c6f4621dc2a2d3ffedc62f
O. C. Christian <chilancer@gmail.com>
2022-06-24 15:42:17 +0100

    initial commit

---
+++ I4G017882CAA/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	author = models.ForeignKey('Post', on_delete=models.CASCADE)
+	created_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	published_date = models.DateTimeField(auto_now=False, auto_now_add=False)
